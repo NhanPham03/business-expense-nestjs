a9b4c57c1c06155ff02f63ac03834d137b87766e
NhanPham03 <ph.nhan03@gmail.com>
2024-08-16 20:51:36 +0700

    Fix projects module

--- src/projects/dto/create-project.dto.ts
+++ src/projects/dto/create-project.dto.ts
@@ -1,15 +1,53 @@
+import { IsArray, IsDateString, IsNotEmpty, IsObject, IsString } from "class-validator";
 import { CreateStaffDto } from "src/staffs/dto/create-staff.dto";
 
-export class CreateProjectDto {
+class StaffSubsetDto {
+  @IsString()
   name: string;
+}
+
+export class CreateProjectDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
   code: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   from: string;
+
+  @IsDateString()
+  @IsNotEmpty()
   to: string;
+
+  @IsObject()
+  @IsNotEmpty()
   project_manager: CreateStaffDto;
+
+  @IsObject()
+  @IsNotEmpty()
   quality_assurance: CreateStaffDto;
+
+  @IsArray()
+  @IsNotEmpty()
   technical_lead: CreateStaffDto[];
+
+  @IsArray()
+  @IsNotEmpty()
   business_analyst: CreateStaffDto[];
+
+  @IsArray()
+  @IsNotEmpty()
   developers: CreateStaffDto[];
+
+  @IsArray()
+  @IsNotEmpty()
   testers: CreateStaffDto[];
+
+  @IsArray()
+  @IsNotEmpty()
   technical_consultant: CreateStaffDto[];
 }
--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,26 +1,35 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { UpdateProjectDto } from './dto/update-project.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
+import { MongoRepository } from 'typeorm';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class ProjectsService {
-  create(createProjectDto: CreateProjectDto) {
-    return 'This action adds a new project';
+  constructor(
+    @InjectRepository(Project)
+    private readonly projectRepository: MongoRepository<Project>,
+  ) {}
+
+  async create(createProjectDto: CreateProjectDto) {
+    return await this.projectRepository.save(createProjectDto);
   }
 
-  findAll() {
-    return `This action returns all projects`;
+  async findAll() {
+    return await this.projectRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} project`;
+  async findOne(id: string) {
+    return await this.projectRepository.findOne({ where: { _id: new ObjectId(id) } });
   }
 
-  update(id: number, updateProjectDto: UpdateProjectDto) {
-    return `This action updates a #${id} project`;
+  async update(id: string, updateProjectDto: UpdateProjectDto) {
+    return await this.projectRepository.update({ _id: new ObjectId(id) }, updateProjectDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} project`;
+  async remove(id: string) {
+    return await this.projectRepository.delete({ _id: new ObjectId(id) });
   }
 }
